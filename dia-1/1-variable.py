435fea8bbae71ab7a3f0b4fc76ddced48e5eeaad
yohan <yohanrr86@gmail.com>
2022-10-10 21:22:56 -0500

    agragacion: agrege los archivos iniciales de python

---
+++ dia-1/1-variable.py
@@ -0,0 +1,5 @@
+edad = 30
+
+print(f"Hola como estas, tu edad es: {edad}")
+
+print(type(edad))
---
+++ dia-1/2-impresion-pantalla.py
@@ -0,0 +1,8 @@
+correo = "yohanrr86@gmail.com"
+
+print(correo)
+
+#la concatenacion por el mas (+) solamente entre entre strings
+
+print('El correo del usuario es',correo)
+
