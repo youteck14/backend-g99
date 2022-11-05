9394bc51a2f946b84b6d8e4f019c5baaf26bb631
yohan <yohanrr86@gmail.com>
2022-11-04 19:32:54 -0500

    Elimine el entorno

---
+++ Dia-2/app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+from os import environ
+#Sirve para ller el archivo .env y cargar las variables definidas en ese archivo como variables de entorno
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#print(environ)
+
+app = Flask(__name__)
+
+# almacena toddas las variables de configuracion de la aplicacion de Flask
+app.config['MYSQL_HOST'] = environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD']=environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(environ.get('MYSQL_PORT'))
+
+#print(app.config)
+mysql = MySQL(app)
+
+
+#un decorador es la forma en la cual nosotros podemos modificar el comportamiento de un metodo de una clase
+#sin la necesidad de modificarlo directamente, es como utilizar la herencia para poder modificar su comportamiento
+#en este caso, dependiendo de su ruta y su metodo
+
+@app.route('/', methods = ['GET'])
+def inicio():
+    return {
+        'message': 'Bienvenidos a mi API de colegios'
+    }
+
+@app.route('/inicio', methods=['GET'])
+def pagina_inicial():
+    return render_template('inicio.html')
+
+
+
+@app.route('/mostrar-alumnos', methods=['GET'])
+def devolver_alumnos():
+    #me crea un conexion con la base de datos
+    cursor = mysql.connection.cursor()
+    #ejecutamos una clausula hacia una determinada tabla
+    cursor.execute("SELECT * FROM alumnos")
+    resultado = cursor.fetchall() #devolver toda la informacion de esa consulta
+    print(resultado)
+    resultado_final = []
+    #itero al resultado de mi consulta a la bd
+    for alumno in resultado:
+        #creo un diccionario para indicar la llave de cada elemento
+        alumno_diccionario = {
+            'id' : alumno[0],
+            'nombre': alumno[1],
+            'ape_paterno': alumno[2],
+            'ape_materno':alumno[3],
+            'correo': alumno[4],
+            'num_emergencia': alumno[5],
+            'curso_id': alumno[6]
+        }
+        print(alumno_diccionario)
+        resultado_final.append(alumno_diccionario)
+
+    #return {
+    #    'message':'Los alumnos son:',
+    #    'content':resultado_final
+    #}
+    return render_template('mostrar_alumnos.html' , alumnos = resultado_final, mensaje='hola desde flask')
+
+@app.route("/agregar-alumno", method=['GET'])
+def agregar_alumno():
+    return render_template('agregar_alumno')
+
+app.run(debug=True)
+
