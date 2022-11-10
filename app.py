509ae61f59aaab7960e358fadeeae7527f309cfa
yohan <yohanrr86@gmail.com>
2022-11-09 22:59:51 -0500

    Eliminando entorno

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from os import environ
 from dotenv import load_dotenv
 from models.usuarios import UsuarioModel
-
+from flask_mysqldb import MySQL
 #para que puedan ser utilizadas como variables de entorno
 load_dotenv()
 
@@ -17,8 +17,8 @@ app.config['MYSQL_PASSWORD'] = environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = environ.get('MYSQL_DB')
 app.config['MYSQL_PORT'] = int(environ.get('MYSQL_PORT'))
 #inicializamos la instancia de Flask-SQLAlchemy con las propiedades seteadas en la aplicacion de flask
-conexion.init_app(app)
-#mysql = MySQL(app)
+#conexion.init_app(app)
+mysql = MySQL(app)
 #Inicializamos la clase Migrate con la configuracion de nuestra base de datos y aplicacion de Flask
 migrate = Migrate(app,conexion)
 
